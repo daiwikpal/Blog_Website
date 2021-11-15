7671e7e4d45cb5068aa7e8c4906c9d4607c47152
daiwikpal <daiwikpal123@gmail.com>
2021-11-15 10:37:51 -0500

    added cards, new font, new color scheme, started working on making card view dynmaic

---
+++ script.js
@@ -0,0 +1,13 @@
+import "./myStyle.css"
+
+
+// array of blog objects
+const blog_array = [ {
+    id: 1,
+    heading: "My First Blog",
+    link: "articleOne.html",
+    date: "2020-01-01",
+    image: "images/default.jpeg",
+    imageAlt: "this is the image alt",
+    content: "This is my first blog post. It is a good one."
+}] 
